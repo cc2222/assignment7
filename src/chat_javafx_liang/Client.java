e0c06d6c5d62e4464fc90ac48138b0f18fc7d073
maxfennis <maxfennis@gmail.com>
2016-11-30 22:09:20 -0600

    updated client and server, w/o groupchat

--- src/chat_javafx_liang/Client.java
+++ src/chat_javafx_liang/Client.java
@@ -16,34 +16,60 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField; 
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage; 
 
 
-public class Client extends Application { 
-	
-	public static int cnt;
-	private String name;
+public class Client extends Application{
+	//private JTextArea incoming; 
+	//private JTextField outgoing; 
+	private BufferedReader reader; 
+	private PrintWriter writer;
+	//private String clientnm;
+	private static int clients;
+	private int recipient = 4;
+	private int sender = 4;
+	private int clientid;
+	private TextArea ta;
+	private TextField tf;
+	private ArrayList<TextArea> allTA;
+	private BorderPane mainPane;
+	private TextArea tA, tG, tB, tCurr;
+	private ScrollPane tap, tAp, tBp, tGp;
+	private boolean both;
 	
 	// IO streams 
 	DataOutputStream toServer = null; 
 	DataInputStream fromServer = null;
 	
-	
 	public Client(){
-		
+		//clientid = clients;
+		//clients++;
 	}
-	public Client(String nm)
-	{
-		name = nm;
+	
+	public Client(String nm){
+		//clientnm = nm;
+		//clientid = clients;
+		//clients++;
+	}
+	
+	public int getClientID(){
+		return clientid;
 	}
 
 	@Override // Override the start method in the Application class 
 	public void start(Stage primaryStage) { 
+		try{
+			setUpNetworking();
+			//clientnm = "Client " + clientid;
+		}catch(Exception e){
+			System.out.println("Error in setUpNetworking()");
+		}
 		
-// Top chat selection
+		// Top chat selection
 		Pane top = new Pane();		
 		
 		//Friends Online
@@ -62,19 +88,15 @@ public class Client extends Application {
 		//get valid Online Friends
         int xCoord = 60;
         ArrayList<String> names = new ArrayList<String>();
-        names.add("Client A");
-        names.add("Client B");
-        names.add("Client C");
+        names.add("Client 0");
+        names.add("Client 1");
+        names.add("Client 2");
         
         ArrayList<RadioButton> radios = new ArrayList<RadioButton>();
         for(String x : names)
         {
-//        	if(x.equals(name))
-//			{
-//        		names.remove(x);
-//			}
-//        	else
-//        	{
+        	if(!(x.equals("Client " + clientid)))
+			{
         		RadioButton cl = new RadioButton();
         		radios.add(cl);
     			cl.setText(x);
@@ -85,18 +107,7 @@ public class Client extends Application {
     	      //cl.setDisable(true);
     	        
     	        top.getChildren().add(cl);
-//        	}
-        }
-        
-        for(RadioButton y : radios)
-        {
-        	y.setOnAction(new EventHandler<ActionEvent>() {
-        		 
-                @Override
-                public void handle(ActionEvent event) {
-                   curr.setText("Chatting with: " + updateActive(radios));
-                }
-            });
+        	}
         }
         
         //Log Off Button  
@@ -117,84 +128,241 @@ public class Client extends Application {
         });
 		
 // Center text area
-		BorderPane mainPane = new BorderPane(); 
-		TextArea ta = new TextArea();		
+        mainPane = new BorderPane();
+		ta = new TextArea();
+		tA = new TextArea();
+		tB = new TextArea();
+		tG = new TextArea();
+		tap = new ScrollPane(ta);
+		tAp = new ScrollPane(tA);
+		tBp = new ScrollPane(tB);
+		tGp = new ScrollPane(tG);
+		
 		
 // Bottom text input - Panel p to hold the label and text field 
 		BorderPane paneForTextField = new BorderPane();
 		paneForTextField.setPadding(new Insets(5, 5, 5, 5)); 
 		paneForTextField.setStyle("-fx-border-color: red"); 
-		TextField tf = new TextField(); 
+		tf = new TextField(); 
 		tf.setPromptText("Send Message...");
 		paneForTextField.setCenter(tf); 
 		
 		//areas for displaying content
 		mainPane.setTop(top);
-		mainPane.setCenter(new ScrollPane(ta));
+		mainPane.setCenter(tap);
 		mainPane.setBottom(paneForTextField);
 
 		// Create a scene and place it in the stage 
 		Scene scene = new Scene(mainPane, 450, 220); 
-		primaryStage.setTitle("Client " + name); // Set the stage title 
+		primaryStage.setTitle("Client " + clientid); // Set the stage title 
 		primaryStage.setScene(scene); // Place the scene in the stage 
 		primaryStage.show(); // Display the stage 
 
 		tf.setOnAction(e -> { 
 			try { 
-				// Get the radius from the text field 
-				double radius = Double.parseDouble(tf.getText().trim()); 
-
-				// Send the radius to the server 
-				toServer.writeDouble(radius); 
-				toServer.flush(); 
-
-				// Get area from the server 
-				double area = fromServer.readDouble(); 
-
-				// Display to the text area 
-				ta.appendText("Radius is " + radius + "\n"); 
-				ta.appendText("Area received from the server is "
-						+ area + '\n');
-
+				
+				if(clientid == 0){
+					if(recipient == 1){
+						tA.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+					if(recipient == 2){
+						tB.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+					if(recipient == 3){
+						tG.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+				}
+				else if(clientid == 1){
+					if(recipient == 0){
+						tA.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+					if(recipient == 2){
+						tB.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+					if(recipient == 3){
+						tG.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+				}
+				else if(clientid == 2){
+					if(recipient == 0){
+						tA.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+					if(recipient == 1){
+						tB.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+					if(recipient == 3){
+						tG.appendText("Client " + clientid + ": " + tf.getText() + "\n");
+					}
+				}
+				
+				writer.println(recipient + "" + sender + "" + clientid + ": " + tf.getText()); 
+				writer.flush();
+				
+				tf.setText(""); 
+				tf.requestFocus();
 			} 
-			catch (IOException ex) { 
+			catch (Exception ex) { 
 				System.err.println(ex); 
 			} 
-		}); 
-
-		try { 
-			// Create a socket to connect to the server 
-			@SuppressWarnings("resource")
-			Socket socket = new Socket("localhost", 8000); 
-			// Socket socket = new Socket("130.254.204.36", 8000); 
-			// Socket socket = new Socket("drake.Armstrong.edu", 8000); 
-
-			// Create an input stream to receive data from the server 
-			fromServer = new DataInputStream(socket.getInputStream()); 
-
-			// Create an output stream to send data to the server 
-			toServer = new DataOutputStream(socket.getOutputStream()); 
-		} 
-		catch (IOException ex) { 
-			ta.appendText(ex.toString() + '\n');
-		}
+		});
+		
+		for(RadioButton y : radios)
+        {
+        	y.setOnAction(new EventHandler<ActionEvent>() {
+        		 
+                @Override
+                public void handle(ActionEvent event) {
+                   curr.setText("Chatting with: " + updateActive(radios));
+                   
+                }
+            });
+        }
 	}
 	
 	public String updateActive(ArrayList<RadioButton> all)
 	{
 		String active = "";
+		int count = 0;
+		both = false;
 		for(RadioButton temp : all)
 		{
 			if(temp.isSelected())
 			{
-				active = active + temp.getText() + " + ";  
+				count++;
+				active = active + temp.getText() + " + ";
+				
+				if(clientid == 0)
+				{
+					if(temp.getText().contains("1"))
+					{
+						mainPane.setCenter(tAp);
+						
+					}
+					if(temp.getText().contains("2"))
+					{
+						mainPane.setCenter(tBp);
+					}
+				}
+				else if(clientid == 1)
+				{
+					if(temp.getText().contains("0"))
+					{
+						mainPane.setCenter(tAp);
+					}
+					if(temp.getText().contains("2"))
+					{
+						mainPane.setCenter(tBp);
+					}
+				}
+				else if(clientid == 2)
+				{
+					if(temp.getText().contains("0"))
+					{
+						mainPane.setCenter(tAp);
+					}
+					if(temp.getText().contains("1"))
+					{
+						mainPane.setCenter(tBp);
+					}
+				}
+				
+				if(count > 1){
+					both = true;
+					recipient = 3;
+				}
+				else{
+					String s = temp.getText();
+					s = s.substring(s.length()-1,s.length());
+					recipient = Integer.parseInt(s);
+				}
 			}	
+		}
+		if(both == true)
+		{
+			mainPane.setCenter(tGp);
 		}
 		if(!active.equals(""))
 		{
 			active = active.substring(0,active.length()-3);
+			return active;
 		}
+     	mainPane.setCenter(tap);
 		return active;
+	}
+	
+	private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource") Socket sock = new Socket("127.0.0.1", 4242); 
+		InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+		reader = new BufferedReader(streamReader); 
+		writer = new PrintWriter(sock.getOutputStream()); 
+		System.out.println("networking established"); 
+		String s = reader.readLine();
+		clientid = Integer.parseInt(s);
+		sender = clientid;
+		Thread readerThread = new Thread(new IncomingReader()); 
+		readerThread.start();
+	}
+	
+	class IncomingReader implements Runnable {
+		public void run() { 
+			String message; 
+			try {
+				while ((message = reader.readLine()) != null) {
+					String s = message.substring(0,2); // 1st recip (0-3), 2nd sender (0-2)
+					message = message.substring(2,message.length());
+					
+					//ta.appendText(message + "\n");
+					//((TextArea) mainPane.getCenter()).appendText(message + "\n");
+					//tCurr.appendText(message + "\n");
+					
+					if(s.substring(0,1).equals("3"))
+					{
+						tG.appendText(message + "\n");
+						
+					}
+					else
+					{
+						if(clientid == 0)
+						{
+							if(s.substring(1,2).equals("1"))
+							{
+								tA.appendText(message + "\n");
+								
+							}
+							if(s.substring(1,2).equals("2"))
+							{
+								tB.appendText(message + "\n");
+							}
+						}
+						else if(clientid == 1)
+						{
+							if(s.substring(1,2).equals("0"))
+							{
+								tA.appendText(message + "\n");
+							}
+							if(s.substring(1,2).equals("2"))
+							{
+								tB.appendText(message + "\n");
+							}
+						}
+						else if(clientid == 2)
+						{
+							if(s.substring(1,2).equals("0"))
+							{
+								tA.appendText(message + "\n");
+							}
+							if(s.substring(1,2).equals("1"))
+							{
+								tB.appendText(message + "\n");
+							}
+						}
+					}
+				}
+			} 
+			catch (IOException ex) { 
+				ex.printStackTrace(); 
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/chat_javafx_liang/MultiThreadServer.java
+++ src/chat_javafx_liang/MultiThreadServer.java
@@ -1,104 +1,96 @@
 package chat_javafx_liang;
 
-import java.io.*;
-import java.net.*;
-import java.util.Date;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class MultiThreadServer {
+	
+	private ArrayList<PrintWriter> clientOutputStreams;
+	private ArrayList<BufferedReader> clientInputStreams;
+	private int clients = 0;
+	private int recipient;
+	private int sender;
 
-public class MultiThreadServer extends Application
-{ // Text area for displaying contents 
-	private TextArea ta = new TextArea(); 
-
-	// Number a client 
-	private int clientNo = 0; 
-
-	@Override // Override the start method in the Application class 
-	public void start(Stage primaryStage) { 
-		// Create a scene and place it in the stage 
-		Scene scene = new Scene(new ScrollPane(ta), 450, 200); 
-		primaryStage.setTitle("MultiThreadServer"); // Set the stage title 
-		primaryStage.setScene(scene); // Place the scene in the stage 
-	//	primaryStage.show(); // Display the stage 
-
-		new Thread( () -> { 
-			try {  // Create a server socket 
-				ServerSocket serverSocket = new ServerSocket(8000); 
-				ta.appendText("MultiThreadServer started at " 
-						+ new Date() + '\n'); 
-
-
-				while (true) { 
-					// Listen for a new connection request 
-					Socket socket = serverSocket.accept(); 
-
-					// Increment clientNo 
-					clientNo++; 
-
-					Platform.runLater( () -> { 
-						// Display the client number 
-						ta.appendText("Starting thread for client " + clientNo +
-								" at " + new Date() + '\n'); 
-
-						// Find the client's host name, and IP address 
-						InetAddress inetAddress = socket.getInetAddress();
-						ta.appendText("Client " + clientNo + "'s host name is "
-								+ inetAddress.getHostName() + "\n");
-						ta.appendText("Client " + clientNo + "'s IP Address is " 
-								+ inetAddress.getHostAddress() + "\n");	}); 
-
-
-					// Create and start a new thread for the connection
-					new Thread(new HandleAClient(socket)).start();
-				} 
-			} 
-			catch(IOException ex) { 
-				System.err.println(ex);
-			}
-		}).start();
+	public static void main(String[] args) {
+		try {
+			new MultiThreadServer().setUpNetworking();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-
-	// Define the thread class for handling
-	class HandleAClient implements Runnable {
-		private Socket socket; // A connected socket
-		/** Construct a thread */ 
-		public HandleAClient(Socket socket) { 
-			this.socket = socket;
+	private void setUpNetworking() throws Exception {
+		clientOutputStreams = new ArrayList<PrintWriter>();
+		clientInputStreams = new ArrayList<BufferedReader>();
+		
+		@SuppressWarnings("resource")
+		ServerSocket serverSock = new ServerSocket(4242);
+		while (true) {
+			Socket clientSocket = serverSock.accept();
+			PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			clientOutputStreams.add(writer);
+			clientInputStreams.add(reader);
+			writer.println(clients);
+			writer.flush();
+			clients++;
+			Thread t = new Thread(new ClientHandler(clientSocket));
+			t.start();
+			System.out.println("got a connection");
 		}
-		/** Run a thread */
-		public void run() { 
-			try {
-				// Create data input and output streams
-				DataInputStream inputFromClient = new DataInputStream( socket.getInputStream());
-				DataOutputStream outputToClient = new DataOutputStream( socket.getOutputStream());
-				// Continuously serve the client
-				while (true) { 
-					// Receive radius from the client 
-					double radius = inputFromClient.readDouble();
+	}
 
-					// Compute area
-					double area = radius * radius * Math.PI; 
-					// Send area back to the client
-					outputToClient.writeDouble(area);
-					Platform.runLater(() -> { 
-						ta.appendText("radius received from client: " +
-								radius + '\n'); 
-						ta.appendText("Area found: " + area + '\n');
-					});
+	private void notifyClients(String message) {
+		String s = message.substring(0,2);
+		message = message.substring(2,message.length());
+		if(recipient == 3){
+			for (PrintWriter writer : clientOutputStreams) {
+				writer.println("Client " + message);
+				writer.flush();
+			}
+			return;
+		}	
+		for(int a = 0; a < clientOutputStreams.size(); a++){
+			if(a == recipient || a == sender){
+				PrintWriter writer = clientOutputStreams.get(a);
+				writer.println(s + "Client " + message);
+				writer.flush();
+			}
+		}
+		
+	}
+
+	class ClientHandler implements Runnable {
+		private BufferedReader reader;
+
+		public ClientHandler(Socket clientSocket) throws IOException {
+			Socket sock = clientSocket;
+			reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		}
+
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					String s = message.substring(0,1);
+					recipient = Integer.parseInt(s);
+					s = message.substring(1,2);
+					sender = Integer.parseInt(s);
+					s = message.substring(2,message.length());
+					//message = message.substring(2,message.length());
+					System.out.println("read " + s);			
+					notifyClients(message);
 				}
-			} catch(IOException e) {
+			} 
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
